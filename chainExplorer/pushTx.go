abd6ac86b3d0b61b1f0be990a37b79bd47608b33
xiaodong liu <xiaodong_lau@yeah.net>
2024-06-14 14:06:55 +0800

    调整pushTx失败返回

--- chainExplorer/pushTx.go
+++ chainExplorer/pushTx.go
@@ -17,19 +17,18 @@ type PushTxResp struct {
 func PushTx(chain string, pushTxDate string) (resp PushTxResp, err error) {
 	url := GetChainExplorerHost() + pushTxPath
 	param := fmt.Sprintf(`{"chainShortName":"%v", "signedTx":"%v"}`, chain, pushTxDate)
-	log.Info().Msg("PushTx param: " + param)
 	_, _, errSli := gorequest.New().Post(url).
 		Set("Content-Type", "application/json").
 		//SendStruct(reqParam).
 		Send(param).
 		EndStruct(&resp)
 	if len(errSli) != 0 {
-		errMsg := fmt.Sprintf("chain explorer PushTx err: %v reqUrl: %v resp: %v", errSli, url, resp)
+		errMsg := fmt.Sprintf("chain explorer PushTx err: %v reqUrl: %v param: %v resp: %v", errSli, url, param, resp)
 		log.Error().Msg(errMsg)
 		return resp, errors.New(fmt.Sprintf("%v", errSli))
 	}
 	if resp.Code != SuccessfulCode {
-		errMsg := fmt.Sprintf("chainExplorer.PushTx err resp.Code != SuccessfulCode,url: %v  resp: %+v", url, resp)
+		errMsg := fmt.Sprintf("chainExplorer.PushTx err resp.Code != SuccessfulCode,url: %v param: %v resp: %+v", url, param, resp)
 		log.Error().Msg(errMsg)
 		return resp, errors.New(resp.Msg)
 	}
